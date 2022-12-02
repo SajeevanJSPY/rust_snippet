2adbbeff1726bfc32695a2cd8e3a8efdadb42f32
Sajeevan <sajeevanjspy@gmail.com>
2022-12-02 17:18:27 +0000

    Created formatter mod | Properly Structured the Files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use rust_snipper::{HTMLElement, HTMLKind, format_a_single_element, creating_the_proper_htmlfile};
+// Modules
 mod output_files;
+mod formatter;
 
+// Getting into the scope
+use formatter::{creating_the_proper_htmlfile, format_a_single_element};
+use rust_snipper::{HTMLElement, HTMLKind};
 use output_files::write_the_html_file;
 
 fn main() {
