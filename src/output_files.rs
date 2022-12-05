abf25ef4935a483b7c277bb05fbbc3804de84575
Sajeevan <sajeevanjspy@gmail.com>
2022-12-05 09:05:11 +0000

    Modified the write_the_html_file Func with if let keyword

--- src/output_files.rs
+++ src/output_files.rs
@@ -2,8 +2,10 @@ use std::fs;
 
 pub fn write_the_html_file(files: String, path: &str) -> String{
     let result = fs::write(path, files);
-    match result {
-        Err(_e) => String::from("Error on writing Files"),
-        Ok(_res) => String::from("Successfully Wrote the File")
+
+    if let Ok(_) = result {
+        String::from("Successfully Wrote the File")
+    } else {
+        String::from("Error on writing Files")
     }
 }
