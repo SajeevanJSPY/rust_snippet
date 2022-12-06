3181838682b0b61664933bf1aeae82882286f5db
Sajeevan <sajeevanjspy@gmail.com>
2022-12-06 14:48:55 +0000

    Checking the file_name exist in the folder or not

--- tests/output_file_control.rs
+++ tests/output_file_control.rs
@@ -7,12 +7,22 @@ mod tests {
     #[test]
     fn check_folder() {
         let folder_name = "output";
-        let file_name = "test.html";
+        let file_name = "index.html";
         let overwrite: bool = true;
 
-        let folder_exist = fs::read_dir(folder_name);
-        if let Ok(files) = folder_exist {
-            println!("Folder Exist");
+        let read_folder = fs::read_dir(folder_name);
+        
+        let mut is_file_exist = false;
+
+        if let Ok(files) = read_folder {
+            for file in files {
+                if let Ok(dir_entry) = file {
+                    if dir_entry.file_name() == file_name {
+                        is_file_exist = true;
+                        break;
+                    }
+                }
+            }
         } else {
             println!("Failed");
         }
