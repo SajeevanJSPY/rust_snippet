34e4ea3b72ae7ba10c520e78b25f51c696e8fbf6
Sajeevan <sajeevanjspy@gmail.com>
2022-12-06 15:30:48 +0000

    Created new FileControl Assoc Func

--- src/output_files.rs
+++ src/output_files.rs
@@ -4,9 +4,16 @@ struct FileControl {
     folder_name: &'static str,
     file_name: &'static str,
     overwrite: bool,
+    is_file_exist: bool
 }
 
-pub fn write_html(files: String, path: &str) -> String{
+impl FileControl {
+    fn new(folder_name: &'static str, file_name: &'static str, overwrite: bool) -> Self {
+        FileControl { folder_name, file_name, overwrite, is_file_exist: false }
+    }  
+}
+
+pub fn write_html(files: String, path: &str) -> String {
     let result = fs::write(path, files);
 
     if let Ok(_) = result {
