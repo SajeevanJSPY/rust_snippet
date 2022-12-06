c3c9e02be320f7bf9d28a3b5ea4a0b892a6c3877
Sajeevan <sajeevanjspy@gmail.com>
2022-12-06 14:31:53 +0000

    Finding the folder_name exist or not

--- tests/output_file_control.rs
+++ tests/output_file_control.rs
@@ -1,7 +1,20 @@
 #[cfg(test)]
+#[allow(dead_code, unused_variables, unused_imports)]
 mod tests {
+    use std::fs;
+
+    // Fields -> Folder Name, File Name, Overwrite
     #[test]
     fn check_folder() {
-            
+        let folder_name = "output";
+        let file_name = "test.html";
+        let overwrite: bool = true;
+
+        let folder_exist = fs::read_dir(folder_name);
+        if let Ok(files) = folder_exist {
+            println!("Folder Exist");
+        } else {
+            println!("Failed");
+        }
     }
 }
