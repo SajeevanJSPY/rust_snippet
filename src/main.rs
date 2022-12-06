a7338717db604bb1b0896a929a02ea0bceb29160
Sajeevan <sajeevanjspy@gmail.com>
2022-12-06 13:23:27 +0000

    Organized and Formatted main.rs File with methods

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use rust_snipper::{HTMLElement, HTMLKind};
 use rust_snipper::formatter::html_file;
 use rust_snipper::output_files::write_html;
+use rust_snipper::{HTMLElement, HTMLKind};
 
 fn main() {
     let path = "output/index.html";
 
-    let h1 = HTMLElement {
-        name: HTMLKind::H1,
-        content: String::from("Hello World"),
-        children: None
-    };
+    let h1 = HTMLElement::new(HTMLKind::H1, String::from("Hello World"), None);
 
     let code = html_file(h1.single_tag());
     write_html(code, path);
