33cfe5672a128418164828ef9b01f8d01eea2dec
Sajeevan <sajeevanjspy@gmail.com>
2022-12-07 14:02:00 +0000

    Explicitly Testing the path exist or not

--- tests/file_control_struct.rs
+++ tests/file_control_struct.rs
@@ -2,11 +2,29 @@
 #[allow(dead_code, unused_variables, unused_imports)]
 
 mod tests {
+    use std::path::Path;
     use rust_snipper::output_files::FileControl;
 
     #[test]
     fn check_file_path() {
-        let mut file = FileControl::new("output_a", "file_a", true);
+        let folder_name = "output";
+        let file_name = "index.html";
+        let mut file = FileControl::new(folder_name, file_name, true);
         file.check_file_path();
+
+        // Explicitly Checking the File Exist Or Not
+        let path = format!("{folder_name}/{file_name}");
+        let file_path = Path::new(&path);
+        assert_eq!(file_path.exists(), true);
+
+        let folder_name2 = "output2";
+        let file_name2 = "index2.html";
+        let mut file2 = FileControl::new(folder_name2, file_name2, true);
+        file2.check_file_path();
+
+        // Explicitly Checking the File Exist Or Not
+        let path2 = format!("{folder_name}/{file_name}");
+        let file_path2 = Path::new(&path);
+        assert_eq!(file_path2.exists(), true);
     }
 }
